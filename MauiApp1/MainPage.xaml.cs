bad2c81ea5c827b43c408bf44e9accaaa195069b
Irrikk <irishnovikova@gmail.com>
2025-05-04 20:54:35 +0700

    Init commit for test

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		  DisplayAlert("Title", "Some new info", "Oki");
+    }
+
+    private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		  Outputlabel.Text = "Вы ввели: " + e.NewTextValue;
+		
+		if(e.NewTextValue == "Font")
+			Outputlabel.FontSize = 30;
+    }
+
+    private void Switch_Toggled(object sender, ToggledEventArgs e)
+    {
+      SwitchLabel.Text = e.Value ? "Switch on" : "Switch off";
+    }
+
+    private void MyPicker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      PickerLabel.Text = MyPicker.SelectedItem.ToString();
+    }
+
+}
+
