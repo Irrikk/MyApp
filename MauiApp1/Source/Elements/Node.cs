9eea7e0649081ebddc0e98a4fca404989f337f82
Nikita Fokin <omnisharp7@gmail.com>
2025-05-04 23:53:02 +0700

    Add base node class

---
+++ MauiApp1/Source/Elements/Node.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1;
+public abstract class Node : Border
+{
+    public Node(Size size, string title, Color titleBarColor, Color nodeColor)
+    {
+        StackLayout layout = new();
+
+        Border titleBar = new() {
+            BackgroundColor = titleBarColor,
+            StrokeShape = new RoundRectangle() {
+                CornerRadius = 8
+            }
+        };
+        layout.Add(titleBar);
+
+        Label label = new();
+        label.HorizontalTextAlignment = TextAlignment.Center;
+        label.Text = title;
+        label.TextColor = new(255, 255, 255, 255);
+        titleBar.Content = label;
+
+        StrokeShape = new RoundRectangle(){
+             CornerRadius = 8
+        };
+
+        Stroke = new LinearGradientBrush()
+        {
+            StartPoint = new(0, 0),
+            EndPoint = new(1, 1),
+            
+        };
+
+        HeightRequest = size.Height;
+        WidthRequest = size.Width;
+
+        BackgroundColor = nodeColor;
+
+        Content = layout;
+    }
+}
---
+++ MauiApp1/Source/Elements/TestNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1;
+
+public class TestNode : Node
+{
+    public TestNode() : base(
+        size: new(100, 50),
+        title: "Test Node",
+        titleBarColor: new(0, 180, 20, 255),
+        nodeColor: new(100, 100, 100, 255))
+    {
+
+    }
+}
