d0cdd6626198ef2b85d415a810799277f44f4c04
Nikita Fokin <omnisharp7@gmail.com>
2025-05-06 00:41:38 +0700

    Add Node selection feature

---
+++ MauiApp1/Source/NodeSelector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public class NodeSelector : IDisposable
+    {
+        private Event<INode> _nodeTouched;
+
+        public INode? CurrentNode { get; private set; }
+        public Event<INode> nodeSelected { get; private set; }
+        public Event<INode> nodeUnselected { get; private set; }
+
+        public NodeSelector(Event<INode> nodeTouched)
+        {
+            _nodeTouched = nodeTouched;
+            nodeTouched.Subscribe(_OnNodeTouched);
+
+            nodeSelected = new();
+            nodeUnselected = new();
+        }
+
+        public void Dispose()
+        {
+            _nodeTouched.Unsubscribe(_OnNodeTouched);
+        }
+
+        private void _OnNodeTouched(INode node)
+        {
+            if(CurrentNode != null) nodeUnselected.Invoke(CurrentNode);
+            CurrentNode = node;
+            nodeSelected.Invoke(node);
+        }
+    }
+}
