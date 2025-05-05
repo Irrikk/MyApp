f6474a7467abf1ad1a58cced352f6b7c72dca99b
Nikita Fokin <omnisharp7@gmail.com>
2025-05-05 19:24:43 +0700

    Add debug utils and event wrapper

---
+++ MauiApp1/Source/Common/Event.cs
@@ -0,0 +1,54 @@
+﻿namespace MauiApp1
+{
+    public class Event
+    {
+        private event Action _event;
+
+        public Event() => _event = () => { };
+
+        public void Subscribe(Action action) => _event += action;
+
+        public void Unsubscribe(Action action) => _event -= action;
+
+        public void Invoke() => _event.Invoke();
+    }
+
+    public class Event<T>
+    {
+        private event Action<T> _event;
+
+        public Event() => _event = (arg) => { };
+
+        public void Subscribe(Action<T> action) => _event += action;
+
+        public void Unsubscribe(Action<T> action) => _event -= action;
+
+        public void Invoke(T arg) => _event.Invoke(arg);
+    }
+
+    public class Event<T1, T2>
+    {
+        private event Action<T1, T2> _event;
+
+        public Event() => _event = (arg1, arg2) => { };
+
+        public void Subscribe(Action<T1, T2> action) => _event += action;
+
+        public void Unsubscribe(Action<T1, T2> action) => _event -= action;
+
+        public void Invoke(T1 arg1, T2 arg2) => _event.Invoke(arg1, arg2);
+    }
+
+    public class Event<T1, T2, T3>
+    {
+        private event Action<T1, T2, T3> _event;
+
+        public Event() => _event = (arg1, arg2, arg3) => { };
+
+        public void Subscribe(Action<T1, T2, T3> action) => _event += action;
+
+        public void Unsubscribe(Action<T1, T2, T3> action) => _event -= action;
+
+        public void Invoke(T1 arg1, T2 arg2, T3 arg3) => _event.Invoke(arg1, arg2, arg3);
+    }
+}
---
+++ MauiApp1/Source/Utils/DebugUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public static class DebugUtils
+    {
+        public static void Print(string message)
+        {
+            Application.Current!.MainPage!.DisplayAlert("Debug message", message, "Ok");
+        }
+    }
+}
