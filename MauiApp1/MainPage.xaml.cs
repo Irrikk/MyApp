439ade112545e77297c8868506f1b973081fc05b
Nikita Fokin <omnisharp7@gmail.com>
2025-05-06 00:44:36 +0700

    Change Main page appearence

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,35 +1,58 @@
-﻿namespace MauiApp1;
+﻿using System;
+
+namespace MauiApp1;
 
 public partial class MainPage : ContentPage
 {
-	
-
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-    private void Button_Clicked(object sender, EventArgs e)
+    public MainPage()
     {
-		  DisplayAlert("Title", "Some new info", "Oki");
+        InitializeComponent();
+
+        AbsoluteLayout layout = new()
+        {
+            BackgroundColor = new(29, 29, 29, 255)
+        };
+
+        Event<INode> nodeTouched = new();
+
+        INode n1 = new TestNode(nodeTouched);
+        n1.Position = new(100, 50f);
+        n1.Title = "First";
+        n1.TitleBarColor = new(135, 49, 49, 255);
+        layout.Add(n1 as IView);
+
+        INode n2 = new TestNode(nodeTouched);
+        n2.Position = new(500, 250f);
+        n2.Title = "Second";
+        n2.TitleBarColor = new(49, 94, 135, 255);
+        layout.Add(n2 as IView);
+
+        Content = layout;
+
+        NodeSelector nodeSelector = new(nodeTouched);
     }
 
-    private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+    private void Button_Clicked(object sender, EventArgs e) // DEL ???
     {
-		  Outputlabel.Text = "Вы ввели: " + e.NewTextValue;
-		
-		if(e.NewTextValue == "Font")
-			Outputlabel.FontSize = 30;
+        DisplayAlert("Title", "Some new info", "Oki");
     }
 
-    private void Switch_Toggled(object sender, ToggledEventArgs e)
+    private void Entry_TextChanged(object sender, TextChangedEventArgs e) // DEL ???
     {
-      SwitchLabel.Text = e.Value ? "Switch on" : "Switch off";
+        Outputlabel.Text = "Вы ввели: " + e.NewTextValue;
+
+        if (e.NewTextValue == "Font")
+            Outputlabel.FontSize = 30;
     }
 
-    private void MyPicker_SelectedIndexChanged(object sender, EventArgs e)
+    private void Switch_Toggled(object sender, ToggledEventArgs e) // DEL ???
     {
-      PickerLabel.Text = MyPicker.SelectedItem.ToString();
+        SwitchLabel.Text = e.Value ? "Switch on" : "Switch off";
+    }
+
+    private void MyPicker_SelectedIndexChanged(object sender, EventArgs e) // DEL ???
+    {
+        PickerLabel.Text = MyPicker.SelectedItem.ToString();
     }
 
 }
--- MauiApp1/Platforms/Android/MainActivity.cs
+++ MauiApp1/Platforms/Android/MainActivity.cs
@@ -1,10 +1,20 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Widget;
 
 namespace MauiApp1;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        Android.Graphics.Color uiFrameColor = new(61, 61, 61, 255);
+        base.OnCreate(savedInstanceState);
+        Window.SetStatusBarColor(uiFrameColor);
+        Window.SetNavigationBarColor(uiFrameColor);
+        //var toolbar = FindViewById<Android.Widget.Toolbar>(Resource.Id.toolbar);
+        //toolbar.SetBackgroundColor(uiFrameColor);
+    }
 }
